22cc1067406a0691acb94f1d60d9988b8da4aafb
Gonzalo Culasso <gonzalo.culasso@gmail.com>
2020-03-05 12:57:27 -0300

    iniciando repo

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { HeroesService } from '../../servicios/heroes.service';
+
+
+
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+})
+export class HeroeComponent {
+
+  heroe: any = {};
+
+
+  constructor( private activatedRouted: ActivatedRoute,
+               private _heroesService: HeroesService
+               
+               ) {
+
+    this.activatedRouted.params.subscribe( params => {
+          
+        this.heroe = this._heroesService.getHeroe ( params ['id']);
+      
+        console.log("Trae heroes"); 
+        
+      }
+
+    )
+
+   }
+
+
+
+  }
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service'; // IMPORTAMOS EL MODULO DE SERVICIO CREADO EN HEROES.SERVICES.TS
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+})
+
+export class HeroesComponent implements OnInit {
+
+  
+  heroes:Heroe[] = [];
+  
+  constructor( private _heroesService: HeroesService,
+        
+    private router:Router 
+    
+    ) { 
+    // console.log("Constructor");
+  
+  }
+
+ ngOnInit() {
+    
+  this.heroes = this._heroesService.getHeroes();
+  // console.log( this.heroes );
+  }
+
+  verHeroe( idx:number) {
+    this.router.navigate([ '/heroe', idx])
+  }
+
+}
